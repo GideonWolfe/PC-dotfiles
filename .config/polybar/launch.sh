b1a70f139a395611db5f9f5299465755a48750e6
GideonWolfe <wolfeg2@wwu.edu>
2019-07-19 10:38:47 -0700

    fixed double bar on main monitor

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
+  done
+else
+  polybar --reload main &
+fi
+
+
+# Launch Polybar, using default config location ~/.config/polybar/config
+#polybar main &
+
+
+echo "Polybar launched..."
