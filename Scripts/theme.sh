0f677d5739eacab2cd1c1cdf14a0ee8d7faaeaf1
Gideon Wolfe <wolfeg2@wwu.edu>
2019-07-26 21:24:32 -0700

    aded checks for installed programs

--- Scripts/theme.sh
+++ Scripts/theme.sh
@@ -22,35 +22,66 @@ echo ""
 
 # Set the colors for telegram-desktop
 # Chat background not setting for some reason, must set manually
-echo "##########################"
-echo "# Updating Telegram Skin #"
-echo "##########################"
-$HOME/.telegram-palette-gen/telegram-palette-gen --wal
-echo ""
+if [ -x "$(command -v telegram-desktop)" ]; then
+    if ! [ test -f "$HOME/.telegram-palette-gen/telegram-palette-gen" ]; then
+        echo "Install telegram palette gen: https://github.com/matgua/telegram-palette-gen"
+    else
+        echo "##########################"
+        echo "# Updating Telegram Skin #"
+        echo "##########################"
+        $HOME/.telegram-palette-gen/telegram-palette-gen --wal
+        echo ""
+    fi
+fi
 
 # Updates steam skin (requires python-wal-steam-git from AUR)
-echo "#######################"
-echo "# Updating Steam Skin #"
-echo "#######################"
-wal_steam -w
-echo ""
+if [ -x "$(command -v wal_steam)" ]; then
+    echo "#######################"
+    echo "# Updating Steam Skin #"
+    echo "#######################"
+    wal_steam -w
+    echo ""
+fi
 
 # Update Keyboard colors
-echo "###########################"
-echo "# Updating Keyboard Color #"
-echo "###########################"
-#KEYBOARDCOLOR=$(cat $HOME/.cache/wal/colors | head -n 3 | tail -n 1 | sed 's/#//')
-KEYBOARDCOLOR=$(cat colors.css | grep color6 | awk '{print $2}' | cut -c2- | rev | cut -c2- | rev)
-echo rgb $KEYBOARDCOLOR > /dev/input/ckb1/cmd
-echo "Keyboard Color Set"
-echo ""
+if [ -x "$(command -v ckb-next)" ]; then
+    echo "###########################"
+    echo "# Updating Keyboard Color #"
+    echo "###########################"
+    KEYBOARDCOLOR=$(cat colors.css | grep color6 | awk '{print $2}' | cut -c2- | rev | cut -c2- | rev)
+    echo rgb $KEYBOARDCOLOR > /dev/input/ckb1/cmd
+    echo "Keyboard Color Set"
+    echo ""
+fi
 
 # Update Spotify colors (requires app restart)
-echo "##########################"
-echo "# Updating Spotify Color #"
-echo "##########################"
-oomoxify-cli  $HOME/.cache/wal/colors-oomox -s /opt/spotify/Apps/
-echo "Spotify Theme Set"
-echo ""
+if [ -x "$(command -v spotify)" ]; then
+    if [ -x "$(command -v oomoxify-cli)" ]; then
+        echo "##########################"
+        echo "# Updating Spotify Color #"
+        echo "##########################"
+        oomoxify-cli  $HOME/.cache/wal/colors-oomox -s /opt/spotify/Apps/
+        echo "Spotify Theme Set"
+        echo ""
+    fi
+fi
 
+# Update Zathura colors (requires Zathura-Pywal)
+if [ -x "$(command -v genzathurarc)" ]; then
+    echo "##########################"
+    echo "# Updating Zathura Color #"
+    echo "##########################"
+    genzathurarc > $HOME/.config/zathura/zathurarc
+    echo "Zathura Theme Set"
+    echo ""
+fi
 
+# Update Gnuplot colors (requires Gnuplot-Pywal)
+if [ -x "$(command -v gengnuplotconfig)" ]; then
+    echo "##########################"
+    echo "# Updating Gnuplot Color #"
+    echo "##########################"
+    gengnuplotconfig > $HOME/.gnuplot
+    echo "Gnuplot Theme Set"
+    echo ""
+fi
