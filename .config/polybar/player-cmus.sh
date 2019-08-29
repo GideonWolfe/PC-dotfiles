5661c3debbb498935e46e2e5f06426f01782968f
GideonWolfe <wolfeg2@wwu.edu>
2019-08-29 16:04:22 -0700

    spotify remote

--- .config/polybar/player-cmus.sh
+++ .config/polybar/player-cmus.sh
@@ -16,9 +16,9 @@ if info=$(cmus-remote -Q 2> /dev/null); then
 		dur_seconds=$(printf "%02d" $(("$duration" % 60)))
 
 		if [ "$status" = "playing" ]; then
-			echo " $artist - $title | $pos_minutes:$pos_seconds / $dur_minutes:$dur_seconds"
+			echo " $artist - $title - $pos_minutes:$pos_seconds / $dur_minutes:$dur_seconds"
 		elif [ "$status" = "paused" ]; then
-			echo " $artist - $title | $pos_minutes:$pos_seconds / $dur_minutes:$dur_seconds"
+			echo " $artist - $title - $pos_minutes:$pos_seconds / $dur_minutes:$dur_seconds"
 		else
 			echo ""
 		fi
